780286da616480d0d5e45de4bc98886299a5bc10
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2022-12-22 09:40:15 +0100

    Report Apple MDM Expiry: - added mailing support - new naming scheme for scheduled RBs

--- runbooks/org_general_report-apple-mdm-cert-expiry_scheduled.ps1
+++ runbooks/org_general_report-apple-mdm-cert-expiry_scheduled.ps1
@@ -1,13 +1,33 @@
+<#
+  .SYNOPSIS
+  Monitor/Report expiry of Apple device management certificates.
+
+  .DESCRIPTION
+  Monitor/Report expiry of Apple device management certificates.
+
+  .NOTES
+  Permissions: 
+  MS Graph (API)
+  - DeviceManagementManagedDevices.Read.All,
+  - DeviceManagementServiceConfig.Read.All,
+  - DeviceManagementConfiguration.Read.All,
+  - Mail.Send
+
+#>
+
 #Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
 
 param(
-    [ValidateScript( { Use-RJInterface -DisplayName "Minimum days left in Cert/Token to be healthy." } )]
-    [int] $Days = 30
+    [int] $Days = 30,
+    [string] $sendAlertTo = "support@glueckkanja-gab.com",
+    # Please make sure this from-Adress exists in Exchange Online
+    [string] $sendAlertFrom = "runbooks@contoso.com"
 )
 
 Connect-RjRbGraph
 
 $minDate = (get-date) + (New-TimeSpan -Day $Days)
+$HTMLBody = ""
 
 $applePushCerts = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/applePushNotificationCertificate" -ErrorAction SilentlyContinue
 if ($applePushCerts) {
@@ -15,8 +35,9 @@ if ($applePushCerts) {
     foreach ($ApplePushCert in $applePushCerts) {
         "## Apple Push Cert '$($ApplePushCert.appleIdentifier)' expiry is/was on $((get-date -date $ApplePushCert.expirationDateTime).ToShortDateString())."
         "## -> $(((get-date -Date $ApplePushCert.expirationDateTime) - (get-date)).Days) days left."
-        if(((get-date -Date $ApplePushCert.expirationDateTime) - $minDate) -le 0) {
+        if (((get-date -Date $ApplePushCert.expirationDateTime) - $minDate) -le 0) {
             "## ALERT - Days left is below limit!"
+            $HTMLBody += "<p><b>Apple Push Certificate '$($ApplePushCert.appleIdentifier)' about to expire: $(((get-date -Date $ApplePushCert.expirationDateTime) - (get-date)).Days) left.</b></p>"
         }
         ""
     }
@@ -30,11 +51,14 @@ if ($vppTokens) {
         if ($token.state -ne 'valid') {
             "## VPP Token for '$($token.appleId)' is not valid."
             "## ALERT - VPP Token not valid!"
-        } else {
+            $HTMLBody += "<p><b>Apple VPP Token '$($token.appleId)' is invalid!</b></p>"
+        }
+        else {
             "## VPP Token for '$($token.appleId)' expiry is/was on $((get-date -date $token.expirationDateTime).ToShortDateString())."
             "## -> $(((get-date -Date $token.expirationDateTime) - (get-date)).Days) days left."
             if (((get-date -date $token.expirationDateTime) - $minDate) -le 0 ) {
                 "## ALERT - Days left is below limit!"
+                $HTMLBody += "<p><b>Apple VPP Token '$($token.appleId)' about to expire: $(((get-date -Date $token.expirationDateTime) - (get-date)).Days) days left. </b></p>"
             }
         }
         ""
@@ -49,7 +73,28 @@ if ($depSettings) {
         "## -> $(((get-date -Date $token.tokenExpirationDateTime) - (get-date)).Days) days left."
         if (((get-date -date $token.tokenExpirationDateTime) - $minDate) -le 0) {
             "## ALERT - Days left is below limit!"
+            $HTMLBody += "<p><b>Apple DEP Settings for '$($token.appleIdentifier)' about to expire: $(((get-date -Date $token.tokenExpirationDateTime) - (get-date)).Days) days left.</b></p>"
         }
         ""
     }
+}
+
+if ($HTMLBody) {
+    "## Alerts found"
+    $message = @{
+        subject = "[Automated eMail] ALERT - Apple Intune integration warnings."
+        body    = @{
+            contentType = "HTML"
+            content     = $HTMLBody
+        }
+    }
+
+    $message.toRecipients = [array]@{
+        emailAddress = @{
+            address = $sendAlertTo
+        }
+    }
+
+    Invoke-RjRbRestMethodGraph -Resource "/users/$sendAlertFrom/sendMail" -Method POST -Body @{ message = $message } | Out-Null
+    "## Alert sent to '$sendAlertTo'."
 }
