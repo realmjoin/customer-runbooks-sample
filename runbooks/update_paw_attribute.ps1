873976c3fb2eabada532a639cbe4f89d851b8bba
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@glueckkanja.com>
2025-01-14 16:26:43 +0100

    migrate to MgGraph Module

--- runbooks/update_paw_attribute.ps1
+++ runbooks/update_paw_attribute.ps1
@@ -12,18 +12,33 @@
 
 #>
 
-#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.8.1" }
+#Requires -Modules Microsoft.Graph.Authentication
 
-Connect-RjRbGraph
+# Handle paging (throttling is already handled by MGGraph)
+# Also - directly returns "values" array
+function Invoke-MgGraphRequestAll {
+    param (
+        [string]$Uri
+    )
+    $result = Invoke-MgGraphRequest -Uri $Uri -Method GET
+    $results = $result.value
+    while ($sresult.'@odata.nextLink') {
+        $nextLink = $sresult.'@odata.nextLink'
+        $result = Invoke-MgGraphRequest -Uri $nextLink -Method GET
+        $results += $result.value
+    }
+    return $results
+}
 
+Connect-MgGraph -Identity
 
 ## Get all Devices
-$allDevices = Invoke-RjRbRestMethodGraph -Resource "/devices" -FollowPaging
+$allDevices = Invoke-MgGraphRequestAll -Uri "https://graph.microsoft.com/v1.0/devices"
 
 ## Search for the PAW Devices 
 foreach ($pawDevice in $allDevices) {
     ## testing purposes, correct to  afterwards
-    if ($pawDevice.physicalIds -eq "[OrderId]:PAW") {
+    if ($pawDevice.physicalIds -contains "[OrderId]:PAW") {
 
         ## print check
         "Physical IDs found for PAW Device: " + $pawDevice.id + ", Display Name: " + $pawDevice.displayName
@@ -36,10 +51,11 @@ foreach ($pawDevice in $allDevices) {
         }
         
         ## Call Graph API with Patch command to update the extensionAttribute
-        Invoke-RjRbRestMethodGraph -Resource "/devices/$($pawDevice.id)" -Body $body -ContentType "application/json" -Method Patch
+        Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/devices/$($pawDevice.id)" -Method PATCH -Body $body -ContentType "application/json"
 
         ## Query Graph API to check if device's extensionAttribute has been set correctly
-        $checkDevice = Invoke-RjRbRestMethodGraph -Resource "/devices/$($pawDevice.id)" -OdSelect "extensionAttributes" -Method Get
+        #$checkDevice = Invoke-RjRbRestMethodGraph -Resource "/devices/$($pawDevice.id)" -OdSelect "extensionAttributes" -Method Get
+        $checkDevice = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/devices/$($pawDevice.id)?`$select=extensionAttributes" -Method GET
         
         ## Print to Terminal
         "## extensionAttribute1 of Device $($pawDevice.displayName) with the DeviceID $($pawDevice.id) has been set to: "
